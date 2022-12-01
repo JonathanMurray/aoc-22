5ca10a619c9957d51cd01afb4a625e51f3cd4cad
Jonathan <jonte.murray@gmail.com>
2022-12-01 20:59:31 +0100

    Solve 1-2

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use std::io::{self, BufRead, BufReader};
 fn main() {
     let file = File::open("input_1.txt").expect("Opening input file");
     let lines = BufReader::new(file).lines();
-    let max_load = puzzle_1_find_most_carried_calories(lines);
-    println!("{}", max_load);
+    let sum = puzzle_1_find_sum_of_top_three(lines);
+    println!("Sum: {}", sum);
 }
 
+#[allow(dead_code)]
 fn puzzle_1_find_most_carried_calories(
     input: impl Iterator<Item = io::Result<impl Into<String>>>,
 ) -> u32 {
@@ -27,15 +28,48 @@ fn puzzle_1_find_most_carried_calories(
     max_elf_load
 }
 
+fn puzzle_1_find_sum_of_top_three(
+    input: impl Iterator<Item = io::Result<impl Into<String>>>,
+) -> u32 {
+    let mut top_three = [0, 0, 0];
+    let mut current_elf_load = 0;
+    for line in input {
+        let line = line.expect("Reading line").into();
+        if line.is_empty() {
+            let (third_index, &third_load) = top_three
+                .iter()
+                .enumerate()
+                .min_by(|(_, a), (_, b)| a.cmp(b))
+                .unwrap();
+            if current_elf_load > third_load {
+                top_three[third_index] = current_elf_load;
+            }
+            current_elf_load = 0;
+        } else {
+            let load: u32 = line.parse().expect("Invalid calory amount on line");
+            current_elf_load += load;
+        }
+    }
+    top_three.iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn simple() {
+    fn most_carried() {
         let lines = vec!["3", "5", "", "6"];
         let lines = lines.into_iter().map(|line| Ok(line));
         let result = puzzle_1_find_most_carried_calories(lines);
         assert_eq!(result, 8);
+    }
+
+    #[test]
+    fn sum_of_top_three() {
+        let lines = vec!["3", "5", "", "6", "", "10", "10", "", "15", ""];
+        let lines = lines.into_iter().map(|line| Ok(line));
+        let result = puzzle_1_find_sum_of_top_three(lines);
+        assert_eq!(result, 43);
     }
 }
