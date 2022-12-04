c657d0efc945d159972b2b8ce4ead1297a95ffe5
Jonathan <jonte.murray@gmail.com>
2022-12-04 12:39:23 +0100

    Solve 4-2

--- src/day_4.rs
+++ src/day_4.rs
@@ -4,17 +4,31 @@ pub fn count_pairs_where_one_fully_contains_the_other(
 ) -> u32 {
     let mut count = 0;
     for line in input {
-        let line = line.into();
-        let (range_a, range_b) = split_on(line, ',');
-        let (start_a, end_a) = parse_range(range_a);
-        let (start_b, end_b) = parse_range(range_b);
-        let full_overlap =
-            (start_b >= start_a && end_b <= end_a) || (start_a >= start_b && end_a <= end_b);
+        let (a, b) = parse_line(line);
+        let full_overlap = (b.0 >= a.0 && b.1 <= a.1) || (a.0 >= b.0 && a.1 <= b.1);
         if full_overlap {
             count += 1;
         }
     }
     count
+}
+
+#[allow(dead_code)]
+pub fn count_overlapping_pairs(input: impl Iterator<Item = impl Into<String>>) -> u32 {
+    let mut count = 0;
+    for line in input {
+        let (a, b) = parse_line(line);
+        let overlap = b.1 >= a.0 && b.0 <= a.1;
+        if overlap {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn parse_line(line: impl Into<String>) -> ((u32, u32), (u32, u32)) {
+    let (range_a, range_b) = split_on(line.into(), ',');
+    (parse_range(range_a), parse_range(range_b))
 }
 
 fn parse_range(range: String) -> (u32, u32) {
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,5 @@ mod day_4;
 fn main() {
     let file = File::open("input_4.txt").expect("Opening input file");
     let lines = BufReader::new(file).lines().map(|line| line.unwrap());
-    println!(
-        "Answer: {}",
-        day_4::count_pairs_where_one_fully_contains_the_other(lines)
-    );
+    println!("Answer: {}", day_4::count_overlapping_pairs(lines));
 }
